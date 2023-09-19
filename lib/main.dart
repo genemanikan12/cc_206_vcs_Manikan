512cc3ccd3665ef53336539e7be7e2942a4bf2cb
genemanikan12 <eugene.manikan@wvsu.edu.ph>
2023-09-19 14:34:30 +0800

    workmanikan

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 // {Maternal Michael Dave C.}: {if you dont play you cant win}
 // {Eugene N. Manikan} : {Aanhin mo ang damo kung kinain kona ang kabayo}
-<<<<<<< Updated upstream
 // {Julian Roman M. Zapanta : {Going gets tough tough gets going}
-=======
-// {Lirg Doan L. Villarde} : {Kumain kana ba?}
->>>>>>> Stashed changes
 // {name3} : {quote3}
 
 import 'package:flutter/material.dart';
